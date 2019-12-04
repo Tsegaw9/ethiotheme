b0fc76db6d40c5e56e94f02f96743c55f8795024
Tsegaw9 <tsegawbaharue@gmail.com>
2019-12-04 17:06:05 +0300

    second commit

---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeenchild' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+
+		<?php get_template_part( 'image' ); ?>
+
+		<?php if ( has_nav_menu( 'top' ) ) : ?>
+			<div class="navigation-top">
+				<div class="wrap">
+					<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
+				</div>
+			</div>
+		<?php endif; ?>
+
+	</header>
+
+	<?php
+
+	/*
+	 * If a regular post or page, and not the front page, show the featured image.
+	 * Using get_queried_object_id() here since the $post global may not be set before a call to the_post().
+	 */
+	if ( ( is_single() || ( is_page() && ! twentyseventeenchild_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
+		echo '<div class="single-featured-image-header">';
+		echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeenchild-featured-image' );
+		echo '</div><!-- .single-featured-image-header -->';
+	endif;
+	?>
+
+	<div class="site-content-contain">
+		<div id="content" class="site-content">
---
+++ template-parts/header/header-image.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<div class="custom-header">
+
+		<div class="custom-header-media">
+			<?php the_custom_header_markup(); ?>
+		</div>
+
+	
+
+</div>
